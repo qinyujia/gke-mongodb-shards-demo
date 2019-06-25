d7087eba13ce93edc3ec50d8c9a0c8d347c76187
yujia.qin <yujia.qin@yitu-inc.com>
2019-06-25 18:22:47 +0800

    remove useless #

--- resources/create.sh
+++ resources/create.sh
@@ -1,7 +1,5 @@
-
 kubectl create secret generic shared-bootstrap-data --from-file=internal-auth-mongodb-keyfile=key.file
 kubectl apply -f mongodb-configdb-service.yaml
 kubectl apply -f mongodb-maindb-service.yaml
 kubectl apply -f mongodb-maindb1-service.yaml
 kubectl apply -f mongodb-mongos-service.yaml
-
